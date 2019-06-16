d2808e9e21898e534a39a634051d8c225baee556
Jayne Carmichael Norrie <39460834+jaynecn@users.noreply.github.com>
2019-06-16 11:31:38 +0100

    LATE SUBMISSION updating TopBar and Header with const styling

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -5,12 +5,52 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <div className="header">
-      <span className="date">SMARCH 32, 2018</span>
-      <h1>Lambda Times</h1>
-      <span className="temp">98°</span>
+    <div style={header}>
+      <span style={headerSpanDate}>SMARCH 32, 2018</span>
+      <h1 style={headerH1}>Lambda Times</h1>
+      <span style={headerSpanTemp}>98°</span>
     </div>
   )
 }
 
+const header = {
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  flexDirection: 'row',
+  backgroundColor: '#fff',
+  borderBottom: '1px solid lightgrey',
+  width: '100%',
+  height: '120px',
+  marginTop: '44px',
+  paddingBottom: '15px'
+}
+
+const headerH1 = {
+  fontSize: '60px',
+  fontFamily: 'Didot, serif',
+  fontWeight: 'bold',
+  flex: '8',
+  textAlign: 'center',
+  color: '#000'
+}
+
+const headerSpanDate = {
+  alignSelf: 'flex-end',
+  fontSize: '11px',
+  fontWeight: 'bold',
+  letterSpacing: '1px',
+  marginLeft: '25px',
+  flex: '1'
+}
+
+const headerSpanTemp = {
+  alignSelf: 'flex-end',
+  fontSize: '11px',
+  fontWeight: 'bold',
+  letterSpacing: '1px',
+  textAlign: 'right',
+  marginRight: '25px',
+  flex: '1'
+}
 export default Header
--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -4,33 +4,90 @@ import styled from 'styled-components';
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
 
+
 const TopBar = () => {
   return (
-    <div className="top-bar">
-      <div className="container">
-        <div className="container-left">
-          <span>TOPICS</span><span>SEARCH</span>
+    <div style={topBar}>
+      <div style={container}>
+        <div style={containerLeft}>
+          <span style={containerLeftSpan}>TOPICS</span><span style={containerLeftSpan}>SEARCH</span>
         </div>
-        <div className="container-center">
-          <span>GENERAL</span><span>BROWNBAG</span><span>RANDOM</span><span>MUSIC</span><span>ANNOUNCEMENTS</span>
+        <div style={containerCenter}>
+          <span style={containerCenterSpan}>GENERAL</span><span style={containerCenterSpan}>BROWNBAG</span><span style={containerCenterSpan}>RANDOM</span><span style={containerCenterSpan}>MUSIC</span><span style={containerCenterSpan}>ANNOUNCEMENTS</span>
         </div>
-        <div className="container-right">
-          <span>LOG IN</span>
+        <div style={containerRight}>
+          <span style={containerRightSpan}>LOG IN</span>
         </div>
       </div>
     </div>
   )
 }
 
-// const Top = styled.div({
-//   width: '100%',
-//   display: 'flex',
-//   justify-content: 'center',
-//   align-items: 'none',
-//   flex-direction: 'row',
-//   position: 'fixed',
-//   height: '44px',
-//   backgroundColor: '#333',
-// });
+
+const topBar = {
+  width: '100%',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'none',
+  flexDirection: 'row',
+  position: 'fixed',
+  height: '44px',
+  backgroundColor: '#333',
+};
+
+const container = {
+  width: '100%',
+  display: 'flex',
+  justifyContent: 'none',
+  alignItems: 'none',
+  flexDirection: 'row',
+  color: '#fff',
+  letterSpacing: '1px',
+  padding: '0 10px'
+}
+
+const containerLeft = {
+  display: 'flex',
+  justifyContent: 'none',
+  alignItems: 'center',
+  flexDirection: 'row',
+  flex: '1',
+  fontSize: '11px'
+}
+
+const containerCenter = {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  flexDirection: 'row',
+  flex: '3',
+  fontSize: '9px'
+}
+
+const containerLeftSpan = {
+  cursor: 'pointer',
+  marginRight: '25%',
+  fontWeight: 'bold'
+}
+
+const containerCenterSpan = {
+  cursor: 'pointer',
+  marginRight: '5%'
+}
+
+const containerRight = {
+  display: 'flex',
+  justifyContent: 'flex-end',
+  alignItems: 'center',
+  flexDirection: 'row',
+  flex: '1',
+  fontSize: '11px',
+  fontWeight: 'bold'
+}
+
+const containerRightSpan = {
+  cursor: 'pointer'
+}
+
 
 export default TopBar;
